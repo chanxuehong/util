201e83eadc16d6916c16dbd15f7086461b280761
chanxuehong <chanxuehong@gmail.com>
2014-05-22 20:43:53 +0800

    no message

--- trim.go
+++ trim.go
@@ -6,7 +6,7 @@ import (
 )
 
 // 去掉 src 开头和结尾的空白, 如果 src 包括换行, 去掉换行和这个换行符两边的空白
-//  NOTE: 根据 '\n' 来分行的, 如果 mac 上的 '\r' 则可以单独写一个
+//  NOTE: 根据 '\n' 来分行的, 某些系统或软件用 '\r' 来分行, 则不能正常工作.
 func TrimSpace(src []byte) []byte {
 	byteSlices := bytes.Split(src, []byte{'\n'})
 	for i := 0; i < len(byteSlices); i++ {
@@ -16,7 +16,7 @@ func TrimSpace(src []byte) []byte {
 }
 
 // 去掉 src 开头和结尾的空白, 如果 src 包括换行, 去掉换行和这个换行符两边的空白
-//  NOTE: 根据 '\n' 来分行的, 如果 mac 上的 '\r' 则可以单独写一个
+//  NOTE: 根据 '\n' 来分行的, 某些系统或软件用 '\r' 来分行, 则不能正常工作.
 func TrimSpaceString(src string) string {
 	strs := strings.Split(src, "\n")
 	for i := 0; i < len(strs); i++ {
