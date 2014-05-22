336b3b97ca2748355f9b0fb0d8aff6a88a21c70e
chanxuehong <chanxuehong@gmail.com>
2014-05-22 17:09:32 +0800

    no message

--- trim.go
+++ trim.go
@@ -1,22 +1,26 @@
 package util
 
 import (
-	"regexp"
+	"bytes"
+	"strings"
 )
 
-var trimSpaceRegexp *regexp.Regexp
-
-func init() {
-	// 匹配文本开头和结尾空白, 或者换行符极其换行符两端的空白
-	trimSpaceRegexp = regexp.MustCompile(`(^\s+)|(\s+$)|([\x20\t\f]*(((\r\n)|\n|\r)[\x20\t\f]*)+)`)
-}
-
 // 去掉 src 开头和结尾的空白, 如果 src 包括换行, 去掉换行和这个换行符两边的空白
+//  NOTE: 根据 '\n' 来分行的, 如果 mac 上的 '\r' 则可以单独写一个
 func TrimSpace(src []byte) []byte {
-	return trimSpaceRegexp.ReplaceAllLiteral(src, nil)
+	byteSlices := bytes.Split(src, []byte{'\n'})
+	for i, byteSlicesLen := 0, len(byteSlices); i < byteSlicesLen; i++ {
+		byteSlices[i] = bytes.TrimSpace(byteSlices[i])
+	}
+	return bytes.Join(byteSlices, nil)
 }
 
 // 去掉 src 开头和结尾的空白, 如果 src 包括换行, 去掉换行和这个换行符两边的空白
+//  NOTE: 根据 '\n' 来分行的, 如果 mac 上的 '\r' 则可以单独写一个
 func TrimSpaceString(src string) string {
-	return trimSpaceRegexp.ReplaceAllLiteralString(src, "")
+	strs := strings.Split(src, "\n")
+	for i, strsLen := 0, len(strs); i < strsLen; i++ {
+		strs[i] = strings.TrimSpace(strs[i])
+	}
+	return strings.Join(strs, "")
 }
