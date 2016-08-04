ba2ce722ac44dd42cfbe07842933738b0b76bcf5
chan <chanxuehong@gmail.com>
2016-08-04 16:31:54 +0800

    up

--- money/money.go
+++ money/money.go
@@ -9,9 +9,15 @@ import (
 	"strings"
 )
 
-type Money int64 // 基本单位: 分
+// Money 表示金钱, 单位为分.
+type Money int64
 
 var _ fmt.Stringer = Money(0)
+
+var (
+	_ encoding.TextMarshaler   = Money(0)
+	_ encoding.TextUnmarshaler = (*Money)(nil)
+)
 
 var (
 	_ json.Marshaler   = Money(0)
@@ -23,26 +29,28 @@ var (
 	_ xml.Unmarshaler = (*Money)(nil)
 )
 
-var (
-	_ encoding.TextMarshaler   = Money(0)
-	_ encoding.TextUnmarshaler = (*Money)(nil)
-)
-
-func (v Money) String() string {
-	text, _ := v.MarshalText()
+// String 将 Money 编码成 xxxx.yz 这样以 '元' 为单位的字符串.
+func (m Money) String() string {
+	text, _ := m.MarshalText()
 	return string(text)
 }
 
-// MarshalJSON 将 Money 编码成 "xxxx.yz" 这样以 元 为单位的字符串.
-func (v Money) MarshalJSON() ([]byte, error) {
-	switch n := int64(v); {
-	case n > 0:
-		str := strconv.FormatInt(n, 10)
+// MarshalText 将 Money 编码成 xxxx.yz 这样以 '元' 为单位的字符串.
+func (m Money) MarshalText() (text []byte, err error) {
+	text, _ = m.MarshalJSON()
+	return text[1 : len(text)-1], nil
+}
+
+// MarshalJSON 将 Money 编码成 "xxxx.yz" 这样以 '元' 为单位的字符串.
+func (m Money) MarshalJSON() (text []byte, err error) {
+	switch {
+	case m > 0:
+		str := strconv.FormatInt(int64(m), 10)
 		switch len(str) {
 		case 1: // x --> "0.0x"
 			bs := make([]byte, 0, 6)
 			bs = append(bs, `"0.0`...)
-			bs = append(bs, str...)
+			bs = append(bs, str[0])
 			bs = append(bs, '"')
 			return bs, nil
 		case 2: // xy --> "0.xy"
@@ -52,26 +60,26 @@ func (v Money) MarshalJSON() ([]byte, error) {
 			bs = append(bs, '"')
 			return bs, nil
 		default: // len(str) >= 3
-			if strings.HasSuffix(str, "00") { // ...x00 --> "...x"
+			if strings.HasSuffix(str, "00") { // xxxx00 --> "xxxx"
 				bs := make([]byte, 0, len(str))
 				bs = append(bs, '"')
 				bs = append(bs, str[:len(str)-2]...)
 				bs = append(bs, '"')
 				return bs, nil
-			} else { // ...xyz --> "...x.yz"
-				bs := make([]byte, 0, len(str)+3)
-				bs = append(bs, '"')
-				bs = append(bs, str[:len(str)-2]...)
-				bs = append(bs, '.')
-				bs = append(bs, str[len(str)-2:]...)
-				bs = append(bs, '"')
-				return bs, nil
 			}
+			// xxxxyz --> "xxxx.yz"
+			bs := make([]byte, 0, len(str)+3)
+			bs = append(bs, '"')
+			bs = append(bs, str[:len(str)-2]...)
+			bs = append(bs, '.')
+			bs = append(bs, str[len(str)-2:]...)
+			bs = append(bs, '"')
+			return bs, nil
 		}
-	case n == 0:
+	case m == 0:
 		return []byte{'"', '0', '"'}, nil
 	default: // n < 0
-		str := strconv.FormatInt(n, 10)
+		str := strconv.FormatInt(int64(m), 10)
 		switch len(str) {
 		case 2: // -x --> "-0.0x"
 			bs := make([]byte, 0, 7)
@@ -86,63 +94,39 @@ func (v Money) MarshalJSON() ([]byte, error) {
 			bs = append(bs, '"')
 			return bs, nil
 		default: // len(str) >= 4
-			if strings.HasSuffix(str, "00") { // -...x00 --> "-...x"
+			if strings.HasSuffix(str, "00") { // -xxxx00 --> "-xxxx"
 				bs := make([]byte, 0, len(str))
 				bs = append(bs, '"')
 				bs = append(bs, str[:len(str)-2]...)
 				bs = append(bs, '"')
 				return bs, nil
-			} else { // -...xyz --> "-...x.yz"
-				bs := make([]byte, 0, len(str)+3)
-				bs = append(bs, '"')
-				bs = append(bs, str[:len(str)-2]...)
-				bs = append(bs, '.')
-				bs = append(bs, str[len(str)-2:]...)
-				bs = append(bs, '"')
-				return bs, nil
 			}
+			// -xxxxyz --> "-xxxx.yz"
+			bs := make([]byte, 0, len(str)+3)
+			bs = append(bs, '"')
+			bs = append(bs, str[:len(str)-2]...)
+			bs = append(bs, '.')
+			bs = append(bs, str[len(str)-2:]...)
+			bs = append(bs, '"')
+			return bs, nil
 		}
 	}
 }
 
-// MarshalXML 将 Money 编码成 xxxx.yz 这样以 元 为单位的字符串.
-func (v Money) MarshalXML(e *xml.Encoder, start xml.StartElement) (err error) {
+// MarshalXML 将 Money 编码成 xxxx.yz 这样以 '元' 为单位的字符串.
+func (m Money) MarshalXML(e *xml.Encoder, start xml.StartElement) (err error) {
 	if err = e.EncodeToken(start); err != nil {
 		return
 	}
-	text, _ := v.MarshalText()
+	text, _ := m.MarshalText()
 	if err = e.EncodeToken(xml.CharData(text)); err != nil {
 		return
 	}
 	return e.EncodeToken(xml.EndElement{Name: start.Name})
 }
 
-// MarshalText 将 Money 编码成 xxxx.yz 这样以 元 为单位的字符串.
-func (v Money) MarshalText() (text []byte, err error) {
-	text, _ = v.MarshalJSON()
-	return text[1 : len(text)-1], nil
-}
-
-// UnmarshalJSON 将 "xxxx.yz" 这样以 元 为单位的字符串解码到 Money 中.
-func (p *Money) UnmarshalJSON(data []byte) (err error) {
-	maxIndex := len(data) - 1
-	if maxIndex < 2 || data[0] != '"' || data[maxIndex] != '"' {
-		return fmt.Errorf("invalid Money JSON text: %s", data)
-	}
-	return p.UnmarshalText(data[1:maxIndex])
-}
-
-// UnmarshalXML 将 xxxx.yz 这样以 元 为单位的字符串解码到 Money 中.
-func (p *Money) UnmarshalXML(d *xml.Decoder, start xml.StartElement) (err error) {
-	var text []byte
-	if err = d.DecodeElement(&text, &start); err != nil {
-		return
-	}
-	return p.UnmarshalText(text)
-}
-
-// UnmarshalText 将 xxxx.yz 这样以 元 为单位的字符串解码到 Money 中.
-func (p *Money) UnmarshalText(text []byte) (err error) {
+// UnmarshalText 将 xxxx.yz 这样以 '元' 为单位的字符串解码到 Money 中.
+func (m *Money) UnmarshalText(text []byte) (err error) {
 	if len(text) == 0 {
 		return fmt.Errorf("invalid Money text: %s", text)
 	}
@@ -167,6 +151,24 @@ func (p *Money) UnmarshalText(text []byte) (err error) {
 	if err != nil {
 		return fmt.Errorf("invalid Money text: %s", text)
 	}
-	*p = Money(n)
+	*m = Money(n)
 	return nil
+}
+
+// UnmarshalJSON 将 "xxxx.yz" 这样以 '元' 为单位的字符串解码到 Money 中.
+func (m *Money) UnmarshalJSON(data []byte) (err error) {
+	maxIndex := len(data) - 1
+	if maxIndex < 2 || data[0] != '"' || data[maxIndex] != '"' {
+		return fmt.Errorf("invalid Money JSON text: %s", data)
+	}
+	return m.UnmarshalText(data[1:maxIndex])
+}
+
+// UnmarshalXML 将 xxxx.yz 这样以 '元' 为单位的字符串解码到 Money 中.
+func (m *Money) UnmarshalXML(d *xml.Decoder, start xml.StartElement) (err error) {
+	var text []byte
+	if err = d.DecodeElement(&text, &start); err != nil {
+		return
+	}
+	return m.UnmarshalText(text)
 }
